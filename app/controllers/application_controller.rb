6e1b3752fefc4e07d54afcc40ee275a0bbe78579
CS70 Hyena <ososaramos@cs.hmc.edu>
2017-09-17 18:25:56 -0700

    Adding audio files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 end
+
+require 'wavefile'
+include WaveFile
+
+#Reader.new("/68437__pinkyfinger__piano-a.wav").each_buffer do |buffer|
+#	puts "Read #{buffer.samples.length} samples."
+#end
