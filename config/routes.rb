5735b75ad89f494012855a22bd437c39a5ce5876
CS70 Hyena <ososaramos@cs.hmc.edu>
2017-09-18 16:07:50 -0700

    Adding bootstrap template

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   # This is my index
   root 'pages#home'
 
-  get 'pages/about'
+  get 'pages/play'
 
   get 'pages/home'
 
-  get 'pages/contact'
+  get 'pages/record'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
